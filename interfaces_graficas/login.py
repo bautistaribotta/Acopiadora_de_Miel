b66c774a4eac7835df0c21e6b7aca2724819c40e
Bautista Ribotta <bautistaribotta@hotmail.com>
2025-11-26 12:39:17 -0300

    Elimino el archivo pruebas.py y reconfiguro la interfaz de inicio de sesion, cambie el uso place() por el uso de grid()

--- interfaces_graficas/login.py
+++ interfaces_graficas/login.py
@@ -15,33 +15,40 @@ def mostrar_login():
     ventana_login = tk.Tk()
     ventana_login.title("Login")
     ventana_login.resizable(False, False)
-    # Llamo a la funcion que deja centrada la ventana
+
+    # LLAMO A LA FUNCION PARA DEJAR LA VENTANA CENTRADA Y CONFIGURO EL GRID
     centrar_ventana(ventana_login, 960, 600)
+    ventana_login.grid_columnconfigure(0, weight=3, minsize=576)  # 60% de 960
+    ventana_login.grid_columnconfigure(1, weight=2, minsize=384)  # 40% de 960
+    ventana_login.grid_rowconfigure(0, weight=1)
+
+    # CONFIGURACION DEL GRID()
+
 
     # IZQUIERDA
     frame_imagen_izquierda = tk.Frame(ventana_login, bg=color_principal)
-    frame_imagen_izquierda.place(x=0, y=0, relwidth=0.6, relheight=1)
+    frame_imagen_izquierda.grid(row=0, column=0, sticky="nsew")
 
     # Espacio para colocar la imagen
 
     # DERECHA
     frame_login_derecha = tk.Frame(ventana_login, bg=color_secundario)
-    frame_login_derecha.place(relx=0.6, y=0, relwidth=0.4, relheight=1)
+    frame_login_derecha.grid(row=0, column=1, sticky="nsew")
 
     label_cartel_bienvenida = tk.Label(frame_login_derecha, text="Inicio de sesion", font="Arial, 35", bg=color_secundario)
-    label_cartel_bienvenida.place(x=30, y=150)
+    label_cartel_bienvenida.grid(row=10, column=5, padx=30, pady=(150, 75))
 
     opciones = ["Administrador", "Usuario"]
     opciones_usuario = ttk.Combobox(frame_login_derecha, values=opciones, state="readonly")
     opciones_usuario.config(font=fuente1)
     opciones_usuario.current(1)
-    opciones_usuario.place(x=74, y=300)
+    opciones_usuario.grid(row=15, column=5, padx=30, pady=10)
 
     entry_contrasenia = ttk.Entry(frame_login_derecha, width=21, font=fuente1, show="*")
-    entry_contrasenia.place(x=76, y=350)
+    entry_contrasenia.grid(row=25, column=5, padx=30, pady=10)
 
     boton_inicio_sesion = ttk.Button(frame_login_derecha, text="Entrar")
-    boton_inicio_sesion.place(x=165, y=400)
+    boton_inicio_sesion.grid(row=50, column=5,padx=30, pady=10)
 
     ventana_login.mainloop()
 
