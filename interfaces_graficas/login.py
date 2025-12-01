54cd27057e6503115183d634bf8935eb1df38ad4
Bautista Ribotta <bautistaribotta@hotmail.com>
2025-12-01 01:58:04 -0300

    Refactorizo algunas lineas de codigo del login

--- interfaces_graficas/login.py
+++ interfaces_graficas/login.py
@@ -28,13 +28,16 @@ def mostrar_login():
     frame_imagen_izquierda = tk.Frame(ventana_login, bg=color_primario)
     frame_imagen_izquierda.grid(row=0, column=0, sticky="nsew")
 
+
     # Espacio para colocar la imagen
+
 
     # DERECHA
     frame_login_derecha = tk.Frame(ventana_login, bg=color_secundario)
     frame_login_derecha.grid(row=0, column=1, sticky="nsew")
 
-    label_cartel_bienvenida = tk.Label(frame_login_derecha, text="Inicio de sesion", font="Arial, 35", bg=color_secundario)
+    label_cartel_bienvenida = tk.Label(frame_login_derecha, text="Inicio de sesion")
+    label_cartel_bienvenida.config(font="Arial, 35", bg=color_secundario)
     label_cartel_bienvenida.grid(row=10, column=5, padx=30, pady=(150, 75))
 
     opciones = ["Administrador", "Usuario"]
