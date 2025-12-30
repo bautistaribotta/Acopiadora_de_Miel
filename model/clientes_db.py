ab10533f80b3c8c346e7e1b97bee999fd4a441b3
Bautista Ribotta <bautistaribotta@hotmail.com>
2025-12-30 11:13:43 -0300

    Creada la funcion de eliminar cliente

--- model/clientes_db.py
+++ model/clientes_db.py
@@ -10,7 +10,7 @@ def buscar_cliente():
     cursor = conexion.cursor()
 
 
-def nuevo_cliente(nombre, apellido, telefono, localidad, direccion, factura_produccion, cuit):
+def insertar_cliente(nombre, apellido, telefono, localidad, direccion, factura_produccion, cuit):
     conexion = mysql.connector.connect(
         host="localhost",
         user="root",
@@ -19,18 +19,19 @@ def nuevo_cliente(nombre, apellido, telefono, localidad, direccion, factura_prod
     )
     cursor = conexion.cursor()
 
-    sql = ("""INSERT INTO clientes (nombre, apellido, telefono, localidad, direccion, factura_produccion, cuit)
-           VALUES (%s, %s, %s, %s, %s, %s, %s)""")
+    instruccion_sql = """INSERT INTO clientes 
+                    (nombre, apellido, telefono, localidad, direccion, factura_produccion, cuit)
+                    VALUES (%s, %s, %s, %s, %s, %s, %s)"""
     valores = (nombre, apellido, telefono, localidad, direccion, factura_produccion, cuit)
 
-    cursor.execute(sql, valores)
+    cursor.execute(instruccion_sql, valores)
     conexion.commit()
 
     cursor.close()
     conexion.close()
 
 
-def eliminar_cliente():
+def eliminar_cliente(id_cliente):
     conexion = mysql.connector.connect(
         host="localhost",
         user="root",
@@ -38,3 +39,13 @@ def eliminar_cliente():
         database="southern_honey_group"
     )
     cursor = conexion.cursor()
+
+    instruccion_sql = "DELETE FROM clientes WHERE ID = %s"
+    # Debe llevar una "," al final para ser tomado como tupla
+    valores = (id_cliente,)
+
+    cursor.execute(instruccion_sql, valores)
+    conexion.commit()
+
+    cursor.close()
+    conexion.close()
